f4a6ab0e532cf6dd08da1688ed2b2d6d70e21456
Z8265 <zhangbo8264@163.com>
2021-01-16 18:24:24 +0800

    update package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,15 @@
 module.exports = {
   root: true,
-  extends: [
-    "airbnb-base",
-    "plugin:vue/essential"
-  ],
-  parserOptions: {
-    "parser": "babel-eslint"
-  },
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',
   env: {
     es6: true,
     node: true,
     browser: true,
-    jest:true
+    jest: true,
   },
   rules: {
-    "no-console": process.env.NODE_ENV !== "production" ? 0 : 2,
-    "max-classes-per-file": 0
-  }
+    'no-console': process.env.NODE_ENV !== 'production' ? 0 : 2,
+    'max-classes-per-file': 0,
+  },
 };
